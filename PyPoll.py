904381af2a18fa5cd735aac87a1a5cb35bbb4331
Sean Barton <seanbarton34@gmail.com>
2022-07-26 19:00:17 -0600

    3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -11,12 +11,6 @@ import os
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-
-    # Print the file object.
-     print(election_data)
-
 #Create a filename variable to a direct or indirect path to the file
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -24,10 +18,29 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 with open(file_to_save, "w") as txt_file:
   txt_file.write("\nArapahoe\nDenver\nJefferson")  
 
+#Set Voter Counter to 0
+total_votes = 0
+
+candidate_options = []
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+
+
 # To do read and analyze data
 with open(file_to_load) as election_data:
   file_reader = csv.reader(election_data)
 
   # Read & Print header row in the CSV file
   headers = next(file_reader)
-  print(headers)
+
+
+  #Print each row in the CSV file
+  for row in file_reader:
+
+    #add to the total vote count.
+    total_votes +=1
+    print(total_votes)
